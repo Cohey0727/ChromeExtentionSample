17110223b16b46806f237d60ebd0956e58925e0d
KoheiOkamoto <ohayousagi.ac.kook0727@gmail.com>
2025-01-28 10:58:17 +0900

    main

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>Chrome Extension123</h1>
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </div>
+  );
+}
+
+export default App;
